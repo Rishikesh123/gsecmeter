69eaa77ab76e153e6084c09e1d6f81419ec8e2b7
RISHIKESH <songra99@gmail.com>
2020-01-08 17:08:38 +0530

    new

---
+++ gsecmeter/gsecWorkStatus/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from gsecWorkStatus.models import  User,UserProfileInfo,agenda
+#
+# # Register your models here.
+admin.site.register(agenda)
+admin.site.register(UserProfileInfo)
+# admin.site.register(Status)
+# admin.site.register(Headline)
+# admin.site.register(User)
